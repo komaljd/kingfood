94072714c7c7c8c9eb1200b5f78619e2471aec64
komal <18jadhavkomal@gmail.com>
2023-09-09 20:01:46 +0530

    First Commit

--- src/brand/MenuItemList.js
+++ src/brand/MenuItemList.js
@@ -2,22 +2,10 @@ import fooditems from "../hotels/FoodItem";
 
 const MenuItemList = ({menuid,catid}) =>{
     console.log('catid is:'+catid);
-    var foodItemListfilter = fooditems
-                            .filter((group) => group.hotel_id == menuid)
-                            .map((group) => ({
-                            ...group,
-                            food_category: group.food_category.filter((item) => item.name),
-                            }));
-    console.log(foodItemListfilter);
-//    var foodItemCatIdMatch = foodItemListfilter.filter(fcatitem =>  (fcatitem.food_category.filter(fcatid =>fcatid.id == catid)));
-
-//    var foodItemCatIdMatch = foodItemListfilter.map(fcatitem => (fcatitem.food_category.filter(fcat=> fcat.id==catid).map(
-//                                                                  items=> <p> {items.id} {items.food_items} </p>)));
-  
-
-    // var foodItemList = foodItemListfilter.map(item=> (item.food_category.map(foodItem =>
-    //      <p key={foodItem.id}>{foodItem.food_items}</p>)));
-
+    //Find the hotel with dynamic hotel id 
+    var foundHotel = fooditems.find((hotel) => hotel.hotel_id == menuid);
+    //Found the food category with dynamic selected food_item Id withing found hotel
+    // car foundCategoryId = foundHotel && 
     return(
         <>
             {foodItemListfilter}
